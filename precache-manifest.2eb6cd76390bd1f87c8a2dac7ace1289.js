08c627c2061d6e31f6f467a08e1d675ca9099b49
Coding With Enjoy <codingwithenjoy@gmail.com>
2023-03-11 19:43:47 +0330

    Updates

---
+++ precache-manifest.2eb6cd76390bd1f87c8a2dac7ace1289.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c35d48f6acb03b240ef4b20ee04d222",
+    "url": "/React-Glassmorphism-Calculator/index.html"
+  },
+  {
+    "revision": "8a5049c893ebca6cbb86",
+    "url": "/React-Glassmorphism-Calculator/static/css/main.5566623f.chunk.css"
+  },
+  {
+    "revision": "55d9828ca1827bb8cae8",
+    "url": "/React-Glassmorphism-Calculator/static/js/2.a5d8ed2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Glassmorphism-Calculator/static/js/2.a5d8ed2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5049c893ebca6cbb86",
+    "url": "/React-Glassmorphism-Calculator/static/js/main.63f61dd5.chunk.js"
+  },
+  {
+    "revision": "102a706bab0f756a441b",
+    "url": "/React-Glassmorphism-Calculator/static/js/runtime-main.a81408d8.js"
+  }
+]);
